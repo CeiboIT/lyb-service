5d007ac7c5c08eb33497ddf16249ab1f15ce2cc5
sebastian <sebastian.fondra@gmail.com>
2015-05-19 23:54:57 -0300

    crud for Users.

--- app/scripts/modules/users/controller.js
+++ app/scripts/modules/users/controller.js
@@ -1,53 +1,26 @@
-(function(){
+(function() {
 
-	'use strict'
+	'use strict';
 
-	var usersListController = function(usersList, auxFunctions) {
-		var list = this;
-
-		//filters
-		
-		list.filters = {
-			username: null
-		}
-
-		//Filter function
-		list.usersFilterFn= function(user) {
-		    if(list.filters.username && user.username && (user.username.toLowerCase().indexOf(list.filters.username.toLowerCase()) == -1) ) {
-		        return false;
-		    };
-
-		    if(list.filters.email && user.email && (user.email.toLowerCase().indexOf(list.filters.email.toLowerCase()) == -1) ) {
-		        return false;
-		    };
-
-		    return true;
-		};
-
-		//Initialization Control
-		function init() {
-			list.users = usersList.data;
-			list.countriesSet = auxFunctions.generateSet(list.users, "country");
-		};
-
-		init();
+	var UserViewController = 
+		function ($templateCache, userService, entityManagerView) {
+		// Controller for the User view
+		var userController = this,
+			opts = {
+	      		entityService: userService,
+		        createTemplate: $templateCache.get('user_create'),
+		        listName: 'Users',
+		        newName: 'Add user',
+		        noResultsText: 'No results found',
+		        confirmText: 'Are you sure?'
+		    };	
+			entityManagerView.createFor(opts)
+				.then(function(entityManager) {
+					userController.entityManager = entityManager;
+				});
 	};
-
-	var userEditorController = function(user, usersService) {
-		var editor = this;
-
-		editor.user = user;
-
-
-		editor.update = function(userToUpdate) {
-			usersService.update(userToUpdate)
-		}
-
-	}
-
+	UserViewController.$inject = ['$templateCache', 'userService', 'entityManagerView'];
 
 	angular.module('users')
-		.controller('usersListController', usersListController)
-		.controller('userEditorController', userEditorController)
-
-})();
+		.controller('UserViewController', UserViewController);
+}());
--- app/scripts/modules/users/service.js
+++ app/scripts/modules/users/service.js
@@ -1,35 +1,11 @@
-(function(){
+(function() {
 
-	'use strict'
-
-	var usersService = function($http, apiBaseUrl, messageBoxService) {
-
-		var usersBaseUrl = apiBaseUrl + '/users';
-
-		this.retrieveAll = function() {
-			return $http.get(usersBaseUrl + '/retrieve-all')
-		}
-
-		this.getUserByName = function(username) {
-			return $http.get(usersBaseUrl + '/get-user-by-username?username='+username)
-				.then(function(data){
-					return data.data;
-				})
-		};
-
-		this.update = function(user) {
-			return $http.post(usersBaseUrl + '/update', user)
-				.then(function(data){
-					if(data.data.err) {
-						messageBoxService.showError('An error has ocurred');
-					} else {
-						messageBoxService.showSuccess('Success saving that')
-					}
-
-				})
-		}
-	}
+	'use strict';
+	
+	var userService = function (entityService) {
+		return entityService.getCrudFor('users');
+	};	
 
 	angular.module('stores')
-		.service('usersService', usersService)
-})();
+		.service('userService', userService);
+}());
