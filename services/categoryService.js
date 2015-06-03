48b52b279c8c34b71e52dd59980095529520522e
sebastian <sebastian.fondra@gmail.com>
2015-06-03 20:02:26 -0300

    add function to retrieve all parents and populate the subcategories

--- services/categoryService.js
+++ services/categoryService.js
@@ -7,21 +7,37 @@ var categoryService = {};
 
 categoryService.create = function(categoryDetails, callback) {
 	var category = new Category(categoryDetails);
+	if (categoryDetails.parent) {
+		category.parent = categoryDetails.parent._id;
+	}
 	logger.log('categoryService.create > ', category);
-	category.save(function(err){
+	category.save(function (err) {
 		if (err) {
 			logger.error('categoryService.create > ' + err);
 			return err;
 		}
-		callback(200);
+		callback(category);
 	});
 };
 
 categoryService.findAll = function(callback) {
-	Category.find({}, function(err, response){
-		if (err) return err;
-	  	callback(response);
-	});
+	Category.find({})
+		.populate('parent')
+		.exec(function(err, response) {
+			if (err) return err;
+	  		callback(response);
+		});
+};
+
+categoryService.findAllParents = function(callback) {
+	Category.find().exists('parent', false)
+		.exec(function(err, response){
+			if (err) {
+				logger.log('error', err);
+				return err;
+			}
+	  		callback(response);
+		});
 };
 
 categoryService.update = function(category, callback) {
@@ -41,5 +57,16 @@ categoryService.getCategoryById = function(categoryId, callback) {
 		callback(response);
 	});
 };
+
+categoryService.getCategoryByTitle = function(title) {
+	var query = Category.findOne({'title': title});
+	return query.exec();
+
+	// function(err, response){
+	// 	if(err) return err;
+	// 	callback(response);
+	// });
+};
+
 module.exports = categoryService;
 
