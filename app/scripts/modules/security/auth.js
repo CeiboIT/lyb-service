72e1d49b9ebdb9a62660347098c90ad98e563700
sebastian <sebastian.fondra@gmail.com>
2015-06-06 22:19:36 -0300

    add identityService, authInterceptor, login and remove mock code.

--- app/scripts/modules/security/auth.js
+++ app/scripts/modules/security/auth.js
@@ -2,29 +2,70 @@
     'use strict';
 
     var AuthService = angular.module('ceibo.auth', []);
-    AuthService.factory('authService', ['$window', '$state', function ($window, $state) {
+    
+    AuthService.factory('authInterceptor', ['$q', '$injector', '$log', 
+        function ($q, $injector, $log) {
+        return {
+            responseError: function (response) {
+                if(response.status === 403) {
+                    $log.debug('403 returned');
+                    $injector.get('$state').go('login');
+                }
+                return $q.reject(response);
+            }
+        };
+    }]);
+
+    AuthService.factory('identityService', ['$log', '$window', '$q',
+        function ($log, $window, $q) {
+        var identity = {},
+            identityService = {
+                setIdentity: function (newIdentity) {
+                    identity = newIdentity;
+                    var identityJson = angular.toJson(newIdentity);
+                    $log.debug('userIdentity: ' + identityJson);
+                    $window.sessionStorage.setItem('lyb.identity', identityJson);
+                },
+                removeIdentity: function () {
+                    identity = undefined;
+                    $window.sessionStorage.removeItem('lyb.identity');
+                },
+                getUserIdentity: function () {
+                    // recupera la identity del user, si no, lanza un reject para ser manejado por el caller.
+                    // Devuelve una promise por si hay que re-loginiar al user.
+                    var deferred = $q.defer();
+                    identity = identity || this.restoreUser();
+                    if (identity) {
+                        deferred.resolve(identity);
+                    } else {
+                        deferred.reject({ error: 'Not logged user'});
+                    }
+                    return deferred.promise;
+                },
+                restoreUser: function () {
+                    // recupera la identity del usuario desde sessionStorage.
+                    var saved = $window.sessionStorage.getItem('lyb.identity');
+                    if (saved) {
+                        $log.debug('user restored ' + saved);
+                        return angular.fromJson(saved);
+                    }
+                    return false; // no estaba la identity en la sesion
+                },
+                isAuthenticated: function() {
+                    return identity || this.restoreUser();
+                }
+            };
+            return identityService;
+    }]);
+
+    AuthService.factory('authService', ['$window', '$state', '$http', 'identityService', 
+        function ($window, $state, $http, identityService) {
         var auth = {
-            loginAsSeller: function (store) { // Mock function to demo. Need to be removed
-                var seller = {
-                    role: 'seller',
-                    username: store.owner.username,
-                    store: store
-                };
-                $window.sessionStorage.setItem('user', JSON.stringify(seller));
-                $state.go('products.list', {}, { reload: true });
-            },
-            loginAsAdmin: function () {
-                var adminUser = {
-                    username: 'Admin',
-                    role: 'admin',
-                };
-                $window.sessionStorage.setItem('user', JSON.stringify(adminUser));
-            },
             isAdmin: function () {
-                return this.getUser().role === 'admin';
+                return auth.getUser().__t === 'Admin';
             },
             getUser: function () {
-                var user = $window.sessionStorage.getItem('user');
+                var user = $window.sessionStorage.getItem('lyb.identity');
                 return JSON.parse(user);
             },
             getStoreId: function () {
@@ -32,6 +73,18 @@
                 if (user && user.store) {
                     return user.store._id;
                 }
+            },
+            login: function (credentials) {
+                return $http.post('/auth/login', credentials)
+                    .then(function (response) {
+                        if (response.data && response.data.identity) { // login ok
+                            identityService.setIdentity(response.data.identity);
+                            return { identity: response.data.identity };
+                        }
+                    });
+            },
+            logout: function () {
+                return $http.post('/auth/logout');
             }
         };
         return auth;
