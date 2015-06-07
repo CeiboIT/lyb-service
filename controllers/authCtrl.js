9eac9b420f0116245d0eb5a80905b20d8dc03d7f
sebastian <sebastian.fondra@gmail.com>
2015-06-06 22:12:01 -0300

    add login, loggedin and logout routes. remove facebook routes

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,17 +1,27 @@
-var passportConf = require('../configs/passport');
+// var passportConf = require('../configs/passport');
 var router = require('express').Router();
 var User = require('../schemas/usersSchema').User;
+var passport = require('passport');
 
 router.post('/credentials', function(req, res){
 	var user = new User();
 	user.findByNameOrEmail(req.body.identification, function(response){
 		res.send(response);
-	})
+	});
+});
 
-})
+router.get('/loggedin', function(req, res) { 
+	res.send(req.isAuthenticated() ? req.user : '0'); 
+});
 
-router.get('/facebook', passportConf.authFacebook);
+router.post('/login', passport.authenticate('local'), function (req, res) {
+	req.session.user = req.user;
+	res.send({ identity: req.user });
+});
 
-router.get('/facebook/callback',  passportConf.facebookCallback);
+router.post('/logout', function(req, res) {
+    req.logout();
+    res.send(200);
+});
 
 module.exports = router;
