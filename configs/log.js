1b6e57eddfa446bc28ff2f75bc9e2741409f5df8
sebastian <sebastian.fondra@gmail.com>
2015-06-06 22:08:19 -0300

    enable prettyprinting

--- configs/log.js
+++ configs/log.js
@@ -2,11 +2,11 @@ var winston = require('winston');
 
 var logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({ json: false, timestamp: true }),
+    new (winston.transports.Console)({ json: false, timestamp: true, prettyPrint: true }),
     new winston.transports.File({ filename: __dirname + '/debug.log', json: false })
   ],
   exceptionHandlers: [
-    new (winston.transports.Console)({ json: false, timestamp: true }),
+    new (winston.transports.Console)({ json: false, timestamp: true, prettyPrint: true }),
     new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
   ],
   exitOnError: false
