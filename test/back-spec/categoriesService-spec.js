b67f817eef2e0892cb12436407fd674500fcb257
sebastian <sebastian.fondra@gmail.com>
2015-06-08 10:24:11 -0300

    refactor to use promises

--- test/back-spec/categoriesService-spec.js
+++ test/back-spec/categoriesService-spec.js
@@ -3,8 +3,7 @@
 	var dbURI    = 'mongodb://localhost/lyb-service-test',
 	  	assert   = require('chai').assert,
 	  	mongoose = require('mongoose'),
-	  	categoryService = require('../../services/categoryService'),
-	  	clearDB  = require('mocha-mongoose')(dbURI);
+	  	categoryService = require('../../services/categoryService');
 
 	describe('create categories', function () {
 		var	parentReq = {
@@ -20,37 +19,36 @@
 		});	
 
 		it('should create a parent category', function (done) {
-			categoryService.create(parentReq, function (response) {
-				if (response === 500) {
-					console.log('create category > error on response');
+			categoryService.create(parentReq)
+				.then(function (response) {
+						console.log(response);
+						assert.equal(response.title, parentReq.title);
+						done();
+				}, function (error) {
+					console.log('create category > error on response ', error);
 					done();
-				} 
-				console.log(response);
-				assert.equal(response.title, parentReq.title);
-				done();
-			});
-		});
+				});
+		}); // it
 
 		it('should create a child category', function (done) {
 			var	req = {
 				title: 'Child',
 				description: 'Another child category'
 			};
-			
 			function createChild () {
 				categoryService.getCategoryByTitle(parentReq.title)
-					.then( function(response) {
+					.then(function(response) {
 						console.log('parent _id: '+ response._id);
 						req.parent = response._id;
-						categoryService.create(req, function (response) {
-							if (response === 500) {
-								console.log('create category > error on response');
-								done();
-							} 
-							console.log('parent of the child: ' + response.parent);
-							assert.equal(response.title, req.title);
-							done();
-						});
+						categoryService.create(req)
+							.then(function (response) {
+									console.log('parent of the child: ' + response.parent);
+									assert.equal(response.title, req.title);
+									done();
+								}, function () {
+									console.log('create category > error on response');
+									done();
+								});
 					}, function (err) {
 						console.log('error ' + err);
 					});
@@ -71,10 +69,12 @@
 						categoryService.create(req, function () {
 							categoryService.findAllParents(function (response) {
 								assert.notNull(response);
+								done();
 							});	
 						});
 					}, function (err) {
 						console.log('error ' + err);
+						done();
 					});
 			}
 			categoryService.create(parentReq, createChild);
