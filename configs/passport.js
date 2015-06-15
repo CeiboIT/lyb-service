43eeea12d44115a8da56f8de7fc405f1a6067443
sebastian <sebastian.fondra@gmail.com>
2015-06-15 17:15:00 -0300

    add facebook-token strategy. Remove facebook-strategy because It won't work service:3000/client:9000 arq

--- configs/passport.js
+++ configs/passport.js
@@ -1,10 +1,24 @@
 var generalConf = require('./general');
 var passportConf = {};
 var logger = require('../configs/log');
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    users = require('../schemas/usersSchema');
-    // FacebookStrategy = require('passport-facebook').Strategy;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var users = require('../schemas/usersSchema');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var FacebookTokenStrategy = require('passport-facebook-token').Strategy;
+
+
+passportConf.isAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    return passport.authenticate('facebook-token') (req, res, next);
+    // res.send(401);
+};
+
+passportConf.authenticate = function (req, res, next) {
+    return passportConf.facebookToken(req, res, next);
+};
 
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -15,6 +29,16 @@ passport.deserializeUser(function(id, done) {
         done(err, user);
     }); 
 });
+
+passportConf.getUser = function (req) {
+    if (req.user) {
+        return req.user;
+    }
+    if (req.session.user) {
+        return req.session.user;
+    }
+    return {};
+};
 
 passportConf.LocalStrategy = passport.use(new LocalStrategy(
     function(username, password, done) {
@@ -40,8 +64,86 @@ passportConf.LocalStrategy = passport.use(new LocalStrategy(
     }));
 
 
+// Facebook
+
+var createFacebookUser = function (profile, token) {
+    // if there is no user found with that facebook id, create them
+    var newUser = new users.Buyer();
+    // set all of the facebook information in our user model
+    newUser.facebook.id    = profile.id; // set the users facebook id                   
+    newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
+    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+    return newUser;
+};
+
+passport.use(new FacebookTokenStrategy({
+        clientID: '822272877864083',
+        clientSecret: '3ce32e04717652e6174aae47d578cdfd' 
+    },
+    function(token, refreshToken, profile, done) {
+        // asynchronous
+        process.nextTick(function() {
+            // find the user in the database based on their facebook id
+            users.Buyer.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                // if there is an error, stop everything and return that
+                // ie an error connecting to the database
+                if (err) {
+                    return done(err);
+                }
+                // if the user is found, then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    var newUser = createFacebookUser(profile, token);
+                    newUser.save(function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                        // if successful, return the new user
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    })
+);
+
+passport.use(new FacebookStrategy({
+        // pull in our app id and secret from our auth.js file
+        clientID: '822272877864083',
+        clientSecret: '3ce32e04717652e6174aae47d578cdfd',
+        callbackURL: 'localhost:9000/auth/facebook/callback'
+    },
+    // facebook will send back the token and profile
+    function(token, refreshToken, profile, done) {
+        // asynchronous
+        process.nextTick(function() {
+            // find the user in the database based on their facebook id
+            users.Buyer.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                // if there is an error, stop everything and return that
+                // ie an error connecting to the database
+                if (err) {
+                    return done(err);
+                }
+                // if the user is found, then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    var newUser = createFacebookUser(profile, token);
+                    newUser.save(function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                        // if successful, return the new user
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
 
 passportConf.session = passport.session();
 passportConf.initialize = passport.initialize();
 
-module.exports = passportConf
+module.exports = passportConf;
