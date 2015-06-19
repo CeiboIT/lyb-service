a53f0c9ec0ee92fdf57c8883396392809c930948
sebastian <sebastian.fondra@gmail.com>
2015-06-19 12:09:20 -0300

    refactor, move from views/users to modules/users

--- app/scripts/modules/users/controller.js
+++ app/scripts/modules/users/controller.js
@@ -2,7 +2,21 @@
 
 	'use strict';
 
-	var UserViewController = 
+	var User = angular.module('users', []);
+
+	User.config(['$stateProvider', function ($stateProvider) {
+		$stateProvider.state('users.list', {
+	        url: '/list',
+	        templateUrl: 'scripts/modules/users/list.html',
+	        controller: 'UserViewController as entityController'
+	    });
+	}]);
+
+	User.run(function(loadTemplate) {
+		loadTemplate('scripts/modules/users/create.html', 'user_create');
+	});
+
+	User.controller('UserViewController', ['$templateCache', 'userService', 'entityManagerView',
 		function ($templateCache, userService, entityManagerView) {
 		// Controller for the User view
 		var userController = this,
@@ -19,13 +33,32 @@
 				.then(function(entityManager) {
 					userController.entityManager = entityManager;
 				});
-	};
-	UserViewController.$inject = ['$templateCache', 'userService', 'entityManagerView'];
+	}]);
 
-	angular.module('users')
-		.run(function(loadTemplate) {
-			loadTemplate('scripts/modules/users/create.html', 'user_create');
-		})
-
-		.controller('UserViewController', UserViewController);
+	User.directive('userView', function () {
+		return {
+			restrict: 'E',
+			scope: {
+				model: '='
+			},
+			templateUrl: 'scripts/modules/users/detail.html',
+			controllerAs: 'userView',
+			controller: function ($scope) {
+				var userView = this;
+				if ($scope.model.facebook) {
+					userView.user = {
+						username: $scope.model.facebook.name,
+						name: $scope.model.facebook.name,
+						email: $scope.model.facebook.email
+					};
+				} else {
+					userView.user = {
+						username: $scope.model.username,
+						name: $scope.model.username,
+						email: $scope.model.email
+					};
+				}
+			}
+		};
+	});
 }());
